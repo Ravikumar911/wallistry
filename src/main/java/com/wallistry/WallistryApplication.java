c5eff71b4df2a4987d691d7419825fb6caad3594
Ravikumar Rajagopalan <ravi.911kumar@gmail.com>
2017-05-25 22:13:20 +0530

    Added project to wallistry git Initial Commit

---
+++ src/main/java/com/wallistry/WallistryApplication.java
@@ -0,0 +1,12 @@
+package com.wallistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallistryApplication.class, args);
+	}
+}
